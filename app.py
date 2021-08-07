c61261ae223a16077d8738aac1f56fc08e8d5045
John <61605789+BigSalmon2@users.noreply.github.com>
2021-08-06 19:01:54 -0500

    Update app.py

--- app.py
+++ app.py
@@ -174,3 +174,4 @@ def get_prediction_mask():
 if __name__ == '__main__':
     from waitress import serve
     serve(app, host='0.0.0.0', port=80)
+    app.run(debug=True, host='0.0.0.0', port=80, threaded=True)
